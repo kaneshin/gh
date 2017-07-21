a9c736badb1794f544bc69578ce9ad64599714a5
Shintaro Kaneko <kaneshin0120@gmail.com>
2017-07-21 17:38:42 +0000

    Add gh files

--- cmd/gh/main.go
+++ cmd/gh/main.go
@@ -4,6 +4,16 @@ import (
 	"flag"
 )
 
+// A Command is an implementation of a gh command.
+type Command struct {
+	Run   func(cmd *Command, args []string)
+	Usage string
+}
+
+var commands = []*Command{
+	cmdMerge,
+}
+
 func init() {
 	flag.Parse()
 }
---
+++ cmd/gh/merge.go
@@ -0,0 +1,9 @@
+package main
+
+var cmdMerge = &Command{
+	Run:   runMerge,
+	Usage: "merge",
+}
+
+func runMerge(cmd *Command, args []string) {
+}
